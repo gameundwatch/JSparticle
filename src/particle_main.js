cfb9d55c8efdcfa2ba13f210d09c6de8ab41e93c
BuildTools <unconfigured@null.spigotmc.org>
2021-09-07 17:21:15 +0900

    add shape shadow

--- src/particle_main.js
+++ src/particle_main.js
@@ -10,17 +10,23 @@ var togglefire = false;
 var counter = 0;
 
 const SPEED = 1;          // particle speed
-const SIZE = 40;           // particle size
+const SIZE = 10;           // particle size
 const MAXLIFE = 2000;     // particle max life
 const SHRINK = 1;         // shrink curve. 1 is liner shrink. if it is higher than 1, shrinking speed is faster. if it is lower, slower. if 0, stable particle.
 const ACCEL = 1;          // particle acceleration curve.
 
-const SHAPE_TYPE = 1;               // shape type : 1 is circle, 2 is line, 3 or higher is polygon. 
+const SHAPE_TYPE = 2;               // shape type : 1 is circle, 2 is line, 3 or higher is polygon. 
 const SHAPE_INSET = 1;              // inside verticle to make star shape. 1 is flat.
 const SHAPE_DEFAULT_ROTATE = 90;    // default shape degree
 const SHAPE_SPIN_SPEED = 4;         // negative value ... clockwise rotation.
-const SHAPE_BODY_COLOR = "white";
-const SHAPE_LINE_COLOR = "cyan";
+const SHAPE_BODY_COLOR = 'rgba(0,0,0,0)';
+const SHAPE_LINE_COLOR = 'rgba(255,0,0,255)';
+
+const SHAPE_SHADOW_OFFSET_X = 0;
+const SHAPE_SHADOW_OFFSET_Y = 0;
+const SHAPE_SHADOW_BLUR = 5;
+const SHAPE_SHADOW_COLOR = 'black';
+
 
 
 // get RandomInt function
@@ -113,7 +119,12 @@ const init = () => {
     SHAPE_DEFAULT_ROTATE,
     SHAPE_SPIN_SPEED,
     SHAPE_BODY_COLOR,
-    SHAPE_LINE_COLOR
+    SHAPE_LINE_COLOR,
+
+    SHAPE_SHADOW_OFFSET_X,
+    SHAPE_SHADOW_OFFSET_Y,
+    SHAPE_SHADOW_BLUR,
+    SHAPE_SHADOW_COLOR
     );
 
 }
--- src/particle_shape.js
+++ src/particle_shape.js
@@ -7,15 +7,31 @@ const shape = {
   type: null, // lower than 1 = "circle", 2 or highter = "polygon"
   inset: null,
   rotate: null,
-  color: null,
+  spin: null,
 
-  setShape: function(type, inset, rotate, spin, body_color, line_color) {
-    this.type = type;
-    this.inset = inset;
+  // COLOR
+  body_color: null,
+  line_color: null,
+
+  // SHADOW
+  shadow_x: null,
+  shadow_y: null,
+  shadow_blur: null,
+  shadow_color: null,
+
+  setShape: function(type, inset, rotate, spin, bodyColor, lineColor, shadowX, shadowY, shadowBlur, shadowColor) {
+    this.type   = type;
+    this.inset  = inset;
     this.rotate = rotate;
-    this.spin = spin;
-    this.body_color = body_color;
-    this.line_color = line_color;
+    this.spin   = spin;
+
+    this.body_color = bodyColor;
+    this.line_color = lineColor;
+
+    this.shadow_x     = shadowX;
+    this.shadow_y     = shadowY;
+    this.shadow_blur  = shadowBlur;
+    this.shadow_color = shadowColor;
   },
 
   drawShape: function(x, y, radius) {
@@ -40,8 +56,15 @@ const shape = {
     }
     ctx.restore();
     ctx.closePath();
+
     ctx.strokeStyle = this.line_color;
     ctx.fillStyle = this.body_color;
+    
+    ctx.shadowOffSetX = this.shadow_x;
+    ctx.shadowOffSetY = this.shadow_y;
+    ctx.shadowBlur    = this.shadow_blur;
+    ctx.shadowColor   = this.shadow_color;
+
     ctx.stroke();
     ctx.fill();
 
